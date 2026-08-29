986acf7781d8160a407ae39cefb25b84e098700e
Bruno Neves <bruno_ntls@hotmail.com>
2026-08-29 09:41:35 -0300

    feat: Implementacao da Vaccine

---
+++ Projeto/backend/src/main/java/br/fai/lds/projetolds2026/ports_and_adapters/adapter/service/vaccine/VaccineServiceAdapter.java
@@ -0,0 +1,61 @@
+package br.fai.lds.projetolds2026.ports_and_adapters.adapter.service.vaccine;
+
+import br.fai.lds.projetolds2026.domain.pet.PetModel;
+import br.fai.lds.projetolds2026.domain.vaccine.VaccineModel;
+import br.fai.lds.projetolds2026.ports_and_adapters.port.dao.pet.PetDao;
+import br.fai.lds.projetolds2026.ports_and_adapters.port.dao.vaccine.VaccineDao;
+import br.fai.lds.projetolds2026.ports_and_adapters.port.service.vaccine.VaccineService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class VaccineServiceAdapter implements VaccineService {
+
+    @Autowired
+    private VaccineModel vaccineModel;
+    @Autowired
+    private VaccineDao vaccineDao;
+    @Autowired
+    private PetDao petDao;
+
+    @Override
+    public int create(VaccineService entity) {
+
+
+        if (vaccineDao == null) {
+            return 0;
+        }
+
+        if (vaccineModel.getName().isEmpty()) {
+            return 0;
+        }
+        
+
+        return 0;
+    }
+
+    @Override
+    public void delete(int id) {
+
+        if (isIdInvalid(id)) {
+            return;
+        }
+        VaccineDao.remove(id);
+
+    }
+
+    @Override
+    public VaccineService findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<VaccineService> findALl() {
+        return List.of();
+    }
+
+    @Override
+    public boolean update(int id, VaccineService entity) {
+        return false;
+    }
+}
---
+++ Projeto/backend/src/main/java/br/fai/lds/projetolds2026/ports_and_adapters/port/dao/vaccine/VaccineDao.java
@@ -0,0 +1,7 @@
+package br.fai.lds.projetolds2026.ports_and_adapters.port.dao.vaccine;
+
+import br.fai.lds.projetolds2026.domain.vaccine.VaccineModel;
+import br.fai.lds.projetolds2026.ports_and_adapters.port.dao.crud.CrudDao;
+
+public interface VaccineDao extends CrudDao <VaccineModel> {
+}
